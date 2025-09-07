d3db41cbe1dfa230d10f664b972d8f32fc11f971
VeseVrellaku <vv71793@ubt-uni.net>
2025-09-07 17:18:58 +0200

    krijimi i Dashboard dhe modifikimi i App.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Dashboard from "./pages/Dashboard"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
-    <>
-      <h1>Landing Page</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/dashboard" element={<Dashboard />} /> 
+      </Routes>
+    </BrowserRouter>
   )
 }
