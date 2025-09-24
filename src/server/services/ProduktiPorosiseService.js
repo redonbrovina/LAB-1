3ac2759eb4892bedb6668ede1589a6b27f3a8bff
elaramosaj <er70517@ubt-uni.net>
2025-09-24 12:27:27 +0200

    Stock fixing
    pasi te blihet nje produkt sasia e stokut te zvogelohet

--- src/server/services/ProduktiPorosiseService.js
+++ src/server/services/ProduktiPorosiseService.js
@@ -1,8 +1,11 @@
 const ProduktiPorosiseRepository = require('../repositories/ProduktiPorosiseRepository');
+const ProduktiService = require('./ProduktiService');
+const { ProduktVariacioni } = require('../models');
 
 class ProduktiPorosiseService {
     constructor() {
         this.repo = new ProduktiPorosiseRepository();
+        this.produktiService = new ProduktiService();
     }
 
     async getAll() {
@@ -16,16 +19,51 @@ class ProduktiPorosiseService {
     }
 
     async create(data) {
+        // Get the product variation to find the main product ID
+        const variation = await ProduktVariacioni.findByPk(data.produkt_variacioniID);
+        if (!variation) {
+            throw new Error('Variacioni i produktit nuk u gjet');
+        }
+
+        // Reduce stock for the main product
+        await this.produktiService.reduceStock(variation.produktiID, data.sasia);
+
+        // Create the order item
         return await this.repo.insert(data);
     }
 
     async update(id, data) {
-        await this.getById(id);
+        const oldItem = await this.getById(id);
+        
+        // If quantity is being updated, adjust stock accordingly
+        if (data.sasia !== undefined && data.sasia !== oldItem.sasia) {
+            const variation = await ProduktVariacioni.findByPk(oldItem.produkt_variacioniID);
+            if (variation) {
+                const quantityDifference = data.sasia - oldItem.sasia;
+                
+                if (quantityDifference > 0) {
+                    // Increasing quantity - reduce stock
+                    await this.produktiService.reduceStock(variation.produktiID, quantityDifference);
+                } else if (quantityDifference < 0) {
+                    // Decreasing quantity - increase stock
+                    await this.produktiService.increaseStock(variation.produktiID, Math.abs(quantityDifference));
+                }
+            }
+        }
+        
         return await this.repo.updateById(id, data);
     }
 
     async delete(id) {
-        await this.getById(id);
+        const item = await this.getById(id);
+        
+        // Get the product variation to find the main product ID
+        const variation = await ProduktVariacioni.findByPk(item.produkt_variacioniID);
+        if (variation) {
+            // Restore stock for the main product
+            await this.produktiService.increaseStock(variation.produktiID, item.sasia);
+        }
+        
         return await this.repo.deleteById(id);
     }
 }
